fb8bffefb21fc474a417865509fe712cc08b4a29
Your Name <daniela.kirilova@gmx.de>
2020-08-26 10:43:40 +0200

    second commit

---
+++ data.js
@@ -0,0 +1,113 @@
+const data = [
+  {
+    questions: "What is the most common colour of toilet paper in France? ",
+    answers: ["Green", "Pink", "Blue", "White"],
+    correctAnswer: "Pink",
+  },
+  {
+    questions: " It's illegal in Texas to put what on your neighbour’s Cow?",
+    answers: ["A bell", "A piercing", "Grafitti", "A hat"],
+    correctAnswer: "Grafitti",
+  },
+  {
+    questions: "True or false: You can sneeze in your sleep",
+    answers: ["True", "False"],
+    correctAnswer: "False",
+  },
+  {
+    questions: "Who invented the word vomit?",
+
+    answers: ["Albert Einstein", " William Shakespeare"],
+    correctAnswer: " William Shakespeare",
+  },
+  {
+    questions: " What is Scooby Doo’s full name? ",
+
+    answers: ["Scooby Dooby", "Scoob Doo", "Scoob Dooby", "Scoobert Doo"],
+    correctAnswer: "Scoobert Doo",
+  },
+  {
+    questions: " Where was the fortune cookie invented?",
+
+    answers: ["Tokyo", "Milan", "Shanghai", "San Francisco"],
+    correctAnswer: "San Francisco",
+  },
+  {
+    questions: "What is the most famous Mexican beer? ",
+
+    answers: ["Beck's", "Corona", "Pacifico", "Modelo"],
+    correctAnswer: "Corona",
+  },
+  {
+    questions: "Which country is the origin of the cocktail Mojito?",
+
+    answers: ["Spain", "Mexico", "Portugal", "Cuba"],
+    correctAnswer: "Cuba",
+  },
+  {
+    questions: "What is Japanese sake made from? ",
+
+    answers: ["Potatoes", "Rice", "Wheat", "Grapes"],
+    correctAnswer: "Rice",
+  },
+  {
+    questions: "Which vitamin is the only one that you will not find in an egg? ",
+
+    answers: ["Vitamin D", "Vitamin A", "Vitamin B", "Vitamin C"],
+    correctAnswer: "Vitamin C",
+  },
+  {
+    questions: "True or false: A woman has walked on the Moon ",
+
+    answers: ["True", "False"],
+    correctAnswer: "False",
+  },
+  {
+    questions: "True or false: According to Scottish law, it is illegal to be drunk in charge of a cow ",
+    answers: ["True", "False"],
+    correctAnswer: "True",
+  },
+  {
+    questions: "True or false: Vietnamese is an official language in Canada ",
+    answers: ["False", "True"],
+    correctAnswer: "False",
+  },
+  {
+    questions: "True or false: The can-opener was not invented until 45 years after the tin can ",
+    answers: ["False", "True"],
+    correctAnswer: "True",
+  },
+  {
+    questions: "True or false:  There are McDonald’s one every continent except one",
+    answers: ["True", "False"],
+    correctAnswer: "False",
+  },,
+  {
+    questions: "True or false: The capital of Libya is Benghazi ",
+    answers: ["False", "True"],
+    correctAnswer: "False",
+  },
+  {
+    questions: "True or false: Goldfish only have a memory of three seconds ",
+    answers: ["True", "False"],
+    correctAnswer: "False",
+  },
+
+  {
+    questions: "True or false: Warsaw is the capital of Bulgaria ",
+    answers: ["True", "False"],
+    correctAnswer: "False",
+  },
+
+  {
+    questions: "True or false: Nicolas Cage and Michael Jackson both married the same woman",
+    answers: ["True", "False"],
+    correctAnswer: "True",
+  },
+
+  {
+    questions: "True or false: An octopus has more than one heart ",
+    answers: ["False", "True"],
+    correctAnswer: "True",
+  },
+];
--- main.js
+++ main.js
@@ -1,1 +1,64 @@
-console.log("hello")
+let counter = 0;
+let score = 0;
+const body = document.getElementsByTagName("body")[0];
+const card = document.createElement("div");
+//score
+let scoreTag = document.createElement("h2");
+scoreTag.innerHTML = "Your points: " + score;
+body.appendChild(scoreTag);
+
+// display   the question and answers
+const questionH = document.createElement("h2");
+card.appendChild(questionH);
+let quest = data[counter].questions;
+questionH.innerHTML = quest;
+
+const answerP = document.createElement("div");
+answerP.className = "answerDiv";
+card.appendChild(answerP);
+
+let answerBtns = "";
+let answer = data[counter].answers;
+answer.forEach((answer) => {
+  answerBtns += `<button id='anwersBtn'>${answer}</button> `;
+
+  answerP.innerHTML = answerBtns;
+});
+
+//write a logic to find the correct answer, if the answer is corect add one point
+ 
+  // let nextQues = document.getElementById("anwersBtn").addEventListener("click", function () {
+
+  //   if (answer.includes(data[counter].correctAnswer)) {
+  //     score += 5;
+  //     answerP.style.color = "green";
+  //     nextBtn();
+  //     return 'That was the correct answer! Well Done!'
+  //   } else {
+  //     answerP.style.color = "red";
+  //     nextBtn();
+  //     return `You should have chosen ${correctAnswer}. `;
+      
+  //   }
+  // });
+ 
+
+// click on button to show next question
+function nextBtn() {
+  const next = document
+    .getElementById("next")
+    .addEventListener("click", function () {
+      //next.removeAttribute("hidden");
+
+      counter = counter + 1;
+      questionH.innerHTML = data[counter].questions;
+      let answerBtns = "";
+      let answer = data[counter].answers;
+      answer.forEach((answer) => {
+        answerBtns += `<button id='anwersBtn'>${answer}</button> `;
+        answerP.innerHTML = answerBtns;
+      });
+    });
+}
+nextBtn()
+body.appendChild(card);
