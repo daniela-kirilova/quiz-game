b1a2197ecb7be5ff72e7184e658361b0ce66054e
Your Name <daniela.kirilova@gmx.de>
2020-08-26 16:48:11 +0200

    almost done

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 let counter = 0;
-let score = 0;
 const body = document.getElementsByTagName("body")[0];
 const card = document.createElement("div");
-//score
-let scoreTag = document.createElement("h2");
-scoreTag.innerHTML = "Your points: " + score;
-body.appendChild(scoreTag);
 
 //question
 const questionH = document.createElement("h2");
@@ -19,51 +14,29 @@ answerP.className = "answerDiv";
 card.appendChild(answerP);
 
 let answerBtns = "";
-
 let answer = data[counter].answers;
-
 answer.forEach((answer) => {
-  answerBtns += `<button class="answersBtn" onclick= "test(${answer})">${answer}</button>`;
+  answerBtns += `<button class="answersBtn" onclick="compare(event)">${answer}</button>`;
+  answerP.innerHTML = answerBtns;
 });
-answerP.innerHTML = answerBtns;
 
-// console.log("answers: ", answer);
-
-function test(el) {
-  el.forEach((item) => {
-    console.log(item);
-  });
+let correct = data[counter].correctAnswer;
+let score = 0;
+let message = document.getElementById("message");
+let scoreTag = document.getElementById("score");
+scoreTag.innerHTML = "Your points: 0";
+function compare(el) {
+  let targetEl = el.target.innerHTML;
+  if (targetEl === correct) {
+    el.target.style.color = "green";
+    message.innerHTML = "You have answered correct!!";
+    scoreTag.innerHTML = "Your points: " + (score += 5);
+    nextBtn();
+  } else {
+    el.target.style.color = "red";
+    message.innerHTML = "You have answered wrong. The correct answer is:  " + correct
+  }
 }
-// let test =document.querySelectorAll('.answersBtn')
-// console.log(test)
-
-
-
-
-
-//find the value of answers when clicked
-// let correct = data[counter].correctAnswer;
-
-// // items.forEach(function (item) {
-// //   item.addEventListener("click",function (element) {
-// //     console.log(element.innerHTML);
-// //   });
-// });
-// function getValue(event){
-// console.log(event.target)
-// }
-
-//find the correct answer, and add one point
-// if (answer.includes(correct)) {
-//   score += 5;
-//   correct.style.color = "green";
-//   return "That was the correct answer! Well Done!";
-//   nextBtn();
-// } else {
-//   correct.style.color = "red";
-//   return `You should have chosen ${correct}. `;
-//   nextBtn();
-// }
 
 // click on button to show next question
 function nextBtn() {
@@ -84,5 +57,5 @@ function nextBtn() {
       });
     });
 }
-nextBtn();
+
 body.appendChild(card);
