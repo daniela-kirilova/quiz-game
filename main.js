d3116c0530e74cacebfffbcda066e9ae365faa77
Your Name <daniela.kirilova@gmx.de>
2020-08-27 10:19:47 +0200

    logic done

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let counter = 0;
+let score = 0;
 const body = document.getElementsByTagName("body")[0];
 const card = document.createElement("div");
-let score = 0;
 body.appendChild(card);
 
-//question
+//questions
 const questionH = document.createElement("h2");
 card.appendChild(questionH);
-let quest = data[counter].questions;
+let question = data[counter].questions;
 let correct = data[counter].correctAnswer;
-questionH.innerHTML = quest;
+questionH.innerHTML = question;
 
 // answers
 const answerP = document.createElement("div");
@@ -17,31 +17,28 @@ answerP.className = "answerDiv";
 card.appendChild(answerP);
 
 let answerBtns = "";
+
 let answer = data[counter].answers;
 answer.forEach((answer) => {
   answerBtns += `<button class="answersBtn" onclick="compare(event)">${answer}</button>`;
   answerP.innerHTML = answerBtns;
 });
-// click on button to show next question
+
 function nextBtn() {
-  const next = document
-    .getElementById("next")
-    .addEventListener("click", function () {
-      let correct = data[counter].correctAnswer;
+  document.getElementById("next").addEventListener("click", function () {
+    let correct = data[counter].correctAnswer;
 
-      questionH.innerHTML = data[counter].questions;
-      console.log("correct", data[counter]);
-      console.log(correct);
-      message.innerHTML = "";
-      message.innerHTML = "";
+    questionH.innerHTML = data[counter].questions;
+ 
+    message.innerHTML = "";
 
-      let answerBtns = "";
-      let answer = data[counter].answers;
-      answer.forEach((answer) => {
-        answerBtns += `<button  class='anwersBtn'  onclick="compare(event)">${answer}</button> `;
-        answerP.innerHTML = answerBtns;
-      });
+    let answerBtns = ""; 
+    let answer = data[counter].answers;
+    answer.forEach((answer) => {
+      answerBtns += `<button  class='anwersBtn'  onclick="compare(event)">${answer}</button> `;
+      answerP.innerHTML = answerBtns;
     });
+  });
   data[counter].correctAnswer;
 }
 
@@ -57,22 +54,21 @@ function compare(el) {
     el.target.style.color = "green";
     message.innerHTML = "You have answered correct!!";
     scoreTag.innerHTML = "Your points: " + (score += 5);
-    // el.target.classList.add("show");
-    console.log(score);
-    if (data.length - 1 === counter && score < 30) {
-      alert("you are stupid");
+   let endMsg= document.createElement("h1")
+   endMsg.className= 'endMsg'
+   card.appendChild(endMsg)
+    if (data.length - 1 === counter && score < 60) {
+     endMsg.innerHTML= "You have " + score + " from 100 points! You can do better!"
+     document;
+  document.getElementById("next").classList.add("hidden");
     }
-    if (data.length - 1 === counter && score > 30) {
-      alert("you are better stupid");
+    if (data.length - 1 === counter  && score > 50) {
+      endMsg.innerHTML= "You have " + score + " from 100 points! Well done!"
+      document.getElementById("next").classList.add("hidden");
     }
-    if (data.length - 1 === counter && score > 60) {
-      alert("you are good");
-    }
+  
     counter = counter + 1;
-  } else {
-    document;
-    // .getElementById("next").classList.add("hidden");
-    // document.getElementById("next").classList.remove("show");
+  } else {   
     el.target.style.color = "red";
     message.innerHTML = "Sorry your answer is wrong, try again! ";
     scoreTag.innerHTML = "Your points: " + (score -= 5);
